f2034d6505e05c06c67dd743be3ba9a6a99f252b
shengui919 <shengui919@outlook.com>
2022-02-27 23:41:24 -0500

    first commit

---
+++ json.php
@@ -0,0 +1,51 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode([
+    'status' => 'success',
+    'feeds' => [
+        'id' => 1,
+        'image' => 'https://wallpaperaccess.com/full/1377110.jpg',
+        'date' => 'February 28, 2022',
+        'Content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+tempore explicabo perspiciatis aspernatur quae quia mollitia quasi?
+Accusamus, tempore? Sit nihil ab nostrum dicta dolorum, aliquid est?
+Praesentium, modi quod. Dicta vel exercitationem accusamus, illo
+cupiditate, explicabo ea deserunt repellat doloremque praesentium eos
+perferendis aliquid corrupti, voluptates tempora amet magnam odit
+ipsam dignissimos sequi unde totam laboriosam. Magnam, hic temporibus.
+Optio ab assumenda, odit incidunt impedit sint excepturi ratione
+laborum. Quod animi consequatur officiis adipisci amet laudantium
+voluptas vitae nulla ratione itaque, ex eos voluptatem tempora modi
+distinctio voluptatum suscipit?',
+        'tags' => [
+            ['title' => 'Event', 'link' => 'https://google.com'],
+            ['title' => 'Event1', 'link' => 'https://google.com']
+        ],
+        'commentCount' => 23,
+        'commentLink' => 'https://google.com',
+        'pinned' => false
+    ],
+    [
+        'id' => 2,
+        'image' => 'https://wallpaperaccess.com/full/1377110.jpg',
+        'date' => 'February 27, 2022',
+        'Content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+tempore explicabo perspiciatis aspernatur quae quia mollitia quasi?
+Accusamus, tempore? Sit nihil ab nostrum dicta dolorum, aliquid est?
+Praesentium, modi quod. Dicta vel exercitationem accusamus, illo
+cupiditate, explicabo ea deserunt repellat doloremque praesentium eos
+perferendis aliquid corrupti, voluptates tempora amet magnam odit
+ipsam dignissimos sequi unde totam laboriosam. Magnam, hic temporibus.
+Optio ab assumenda, odit incidunt impedit sint excepturi ratione
+laborum. Quod animi consequatur officiis adipisci amet laudantium
+voluptas vitae nulla ratione itaque, ex eos voluptatem tempora modi
+distinctio voluptatum suscipit?',
+        'tags' => [
+            ['title' => 'Event', 'link' => 'https://google.com'],
+            ['title' => 'Event1', 'link' => 'https://google.com']
+        ],
+        'commentCount' => 23,
+        'commentLink' => 'https://google.com',
+        'pinned' => true
+    ]
+]);
